30c91e74c49155c1bf9e7b7537567527b3e24b11
FieryTornado <priscillaymensah@gmail.com>
2017-07-16 19:00:55 +0200

    Add files via upload

---
+++ JavaBikesGit/JavaBikesSummer/src/JavaBikesGit/JavaBikesSummer/src/JavaBikesGit/JavaBikesSummer/src/control/CustomerCtrl.java
@@ -0,0 +1,27 @@
+package control;
+
+import java.util.ArrayList;
+
+import model.*;
+import view.*;
+
+public class CustomerCtrl 
+{
+	Control customerCtrl = new Control();
+	
+	private ArrayList<Customer>CustomerList;
+	private ArrayList<Product>ProductList;
+	
+	public static void CreateCustomer() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void Login() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
